f8e019c7c2406dce970ee4207d7a374279431e22
thanhbinh163 <binhipadair@gmail.com>
2016-12-21 15:27:02 +0700

    thêm project mẫu + hướng dẫn tạo module

---
+++ TB/TB.MvcWeb/Areas/NhaXuatBan/NhaXuatBanAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace NhaXuatBan.Controllers
+{
+    public class NhaXuatBanAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "NhaXuatBan";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "NhaXuatBan_default",
+                "NhaXuatBan/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "NhaXuatBan.Controllers" }
+            );
+        }
+    }
+}
---
+++ TB/TB.MvcWeb/Areas/ProjectMauName/HangHoaAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace HangHoa.Controllers
+{
+    public class HangHoaAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "HangHoa";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "HangHoa_default",
+                "HangHoa/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "HangHoa.Controllers" }
+            );
+        }
+    }
+}

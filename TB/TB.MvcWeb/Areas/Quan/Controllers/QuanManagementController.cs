2f1d0fd8b35bab93e694dc294f8221b1a7b33a63
trantien489 <tranviettien1996@gmail.com>
2016-12-29 19:55:20 +0700

    Module Quan, chưa có PopupDSTinhThanhPho

---
+++ TB/TB.MvcWeb/Areas/Quan/Controllers/QuanManagementController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TB.Domain;
+using TB.QuanRepo;
+
+namespace Quan.Controllers
+{
+    public class QuanManagementController : Controller
+    {
+        public ActionResult Index()
+        {         
+            return View();
+        }
+        public ActionResult ListQuan(QuanSearchRepository repo)
+        {
+            repo.Page = 1;
+            repo.Execute(this);
+            return View();
+        }
+        public ActionResult InsertQuan(string ParentId)
+        {
+            ViewBag.ParentId = ParentId;
+            return View();
+        }
+        
+        public ActionResult UpdateQuan(QuanGetByIdRepository repo)
+        {
+            repo.Execute(this);
+            return View();
+        }
+
+        //API
+        [HttpPost]
+        public ActionResult ExecuteSearch(QuanSearchRepository repo)
+        {
+            var list = repo.Execute(this);
+            return Json(list);
+        }
+        [HttpPost]
+        public ActionResult ExecuteUpdateQuan(QuanUpdateRepository repo)
+        {
+            return Json(repo.Execute(this));
+        }
+
+        [HttpPost]
+        public ActionResult ExecuteInsertQuan(QuanInsertRepository repo)
+        {
+            return Json(repo.Execute(this));
+        }
+
+        [HttpPost]
+        public ActionResult ExecuteDeleteQuan(QuanDeleteRepository repo)
+        {
+            return Json(repo.Execute(this));
+        }
+        
+    }
+}
---
+++ TB/TB.QuanRepo/QuanSearchRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TB.BaseRepo;
+using TB.Domain.EntityModel;
+using X.PagedList;
+namespace TB.QuanRepo
+{
+    public class QuanSearchRepository : BaseRepository<dynamic>
+    {
+        public Quan Quan { get; set; }
+        public override Result<dynamic> ExecuteCore(Controller CurrentContext)
+        {
+            #region Get List
+            if (Quan == null || Extensions.DeepEquals(Quan, new Quan()))
+            {
+                var tmp = db.Quans.ToPagedListForEntityFramework(s => s.QuanId, Convert.ToInt32(Page), PageSize);
+                var data = new
+                {
+                    Data = tmp.ToList(),
+                    PageCount = tmp.PageCount,
+                    Page = tmp.PageNumber,
+                };
+                return Success(data);
+
+            }
+            #endregion
+
+            #region Search
+            var result = db.Quans.Where(
+                p =>
+                p.Ten.Contains(Quan.Ten) ||                
+                p.TinhThanhPhoId == Quan.TinhThanhPhoId
+                );
+            var page = result.ToPagedListForEntityFramework(s => s.QuanId, Convert.ToInt32(Page), PageSize);
+            var dataSearch = new
+            {
+                Data = page.ToList(),
+                PageCount = page.PageCount,
+                Page = page.PageNumber,
+            };
+            return Success(dataSearch);
+
+            #endregion
+        }
+    }
+}

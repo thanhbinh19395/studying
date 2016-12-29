cefe06a240eaaa25334bd6a092f4606325e2d16f
trantien489 <tranviettien1996@gmail.com>
2016-12-30 01:04:12 +0700

    WebUser: Load dữ liệu Hàng hóa (LinkTest : /Home/Tientest)

--- TB/TB.MvcWebUser/Controllers/HomeController.cs
+++ TB/TB.MvcWebUser/Controllers/HomeController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TB.Domain.EntityModel;
 
 namespace TB.MvcWebUser.Controllers
 {
     public class HomeController : Controller
     {
+        private QLBH db = new QLBH();
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult Tientest()
+        {
+            return View(db.HangHoas.ToList());
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";

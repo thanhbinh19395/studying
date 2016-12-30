dd5adaa74522cf32cde6fe7ffcc7a4eb67807ea0
trantien489 <tranviettien1996@gmail.com>
2016-12-31 01:05:34 +0700

    Thêm giohang, Chưa hoàn thiện chỉnh sửa giỏ hàng

--- TB/TB.MvcWebUser/Controllers/HomeController.cs
+++ TB/TB.MvcWebUser/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TB.Domain.EntityModel;
+using TB.MvcWebUser.Models;
 
 namespace TB.MvcWebUser.Controllers
 {
@@ -29,6 +30,68 @@ namespace TB.MvcWebUser.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        [HttpPost]
+        public void AddCart(int HanghoaId)
+        {
+            //Lấy lại danh sách sách chọn ở trong Session
+            List<CartItem> giohang = new List<CartItem>();
+            if (Session["GioHang"] != null)
+            {
+                giohang = Session["GioHang"] as List<CartItem>;
+            }
+
+            //tìm xem đã có sách trong giỏ hàng
+            CartItem item = giohang.SingleOrDefault(p => p.ProductOrder.HangHoaId == HanghoaId);
+            if (item != null)
+            {
+                item.Quantity++;
+            }
+            else
+            {
+                giohang.Add(new CartItem
+                {
+                    ProductOrder = db.HangHoas.Find(HanghoaId),
+                    Quantity = 1
+                });
+            }
+
+            //ghi nhận Session
+            Session["GioHang"] = giohang;
+            //trả về tổng số lượng hàng hòa            
+
+            
+        }
+        [HttpPost]
+        public JsonResult getInfoCart()
+        {
+            List<CartItem> giohang = Session["GioHang"] as List<CartItem>;
+            if (giohang != null)
+            {
+                return Json(
+                    new
+                    {
+                        ItemCount = giohang.Sum(p => p.Quantity),
+                        Total = Convert.ToDouble(giohang.Sum(p => p.Quantity * p.ProductOrder.GiaBanThamKhao)).ToString("N0"),
+                        Giohang = giohang.ToList()
+                    }
+                );
+            }
+            else {
+                return Json(
+                    new
+                    {
+                        ItemCount = 0,
+                        Total = 0,
+                        //Giohang = giohang.ToList()
+                    }
+                );
+
+            }
+        }
+        public ActionResult ViewCarts()
+        {
             return View();
         }
     }
---
+++ TB/TB.MvcWebUser/Scripts/giohang.js
@@ -0,0 +1,56 @@
+﻿var obj = {
+    getInfoCart: function(){
+        $.ajax({
+            type: "POST",
+            url: "/Home/getInfoCart",
+            success: function (data) {
+                console.log(data);
+                $('#ItemCount').html(data.ItemCount);
+                $('.Total').html(data.Total + ' Vnd');
+                var s = '';
+                var htmlBigCart = '';
+                $.each(data.Giohang, function (key, value) {
+                    s += "<li class='item'>";
+                    s += "  <a href='product_default.html' class='preview-image'><img class='preview' src='/Content/ip7.jpg' alt=''></a>";
+                    s += "  <div class='description'> <a href='#'>" + value.ProductOrder.Ten + "</a> <strong class='price'>" + value.Quantity + " x "+ value.ProductOrder.GiaBanThamKhao + "</strong> </div>";
+                    s += "</li>";
+
+                    htmlBigCart+= "<tr>";
+                    htmlBigCart+=    "<td><a href='#' class='remove-button hidden-xs'><span class='icon-cancel-2 '></span></a></td>";
+                    htmlBigCart+=    "<td><a href='#' class='remove-button visible-xs'><span class='icon-cancel-2 '></span></a><a href=''><img class='preview' src='/Content/ip7.jpg'></a></td>";
+                    htmlBigCart+=    "<td><span class='td-name visible-xs'>Product</span><a href='#'>"+ value.ProductOrder.Ten +"</a></td>";
+                    htmlBigCart += "<td><span class='td-name visible-xs'>Price</span>" + value.ProductOrder.GiaBanThamKhao + "</td>";
+                    htmlBigCart+=    "<td>";
+                    htmlBigCart+=       "<span class='td-name visible-xs'>Quantity</span><div class='input-group quantity-control'>";
+                    htmlBigCart+=       "<span class='input-group-addon'>&minus;</span>";
+                    htmlBigCart +=      "<input type='text' class='form-control' value='" + value.Quantity + "'>";
+                    htmlBigCart+=       "<span class='input-group-addon'>+</span>";
+                    htmlBigCart+=       "</div>";
+                    htmlBigCart+=     "</td>"
+                    htmlBigCart += "<td><span class='td-name visible-xs'>Total</span>" + (value.Quantity * value.ProductOrder.GiaBanThamKhao) + "</td>";
+                    htmlBigCart+=  "</tr>";    
+                })
+                $('#ListCartitem').html(s);
+                $('#ListCartitem2').html(htmlBigCart);
+                                
+                
+            }
+        });
+    },
+}
+$(document).ready(function () {
+    obj.getInfoCart();
+});
+
+
+function AddCart(HanghoaId) {
+    $.ajax({
+        type: "POST",
+        url: "/Home/AddCart",
+        data: { HanghoaId: HanghoaId },
+        success: function (data) {
+            alert('Đã thêm vào giỏ hàng');
+            obj.getInfoCart();
+        }
+    });
+};

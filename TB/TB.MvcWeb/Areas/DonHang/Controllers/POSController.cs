887ba07f37ade3a66edae95aa4dd58a5f52e5e04
thanhbinh163 <binhipadair@gmail.com>
2017-01-03 22:28:41 +0700

    POS unfinished

---
+++ TB/TB.MvcWeb/Areas/DonHang/Controllers/POSController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using TB.HoaDonRepo;
+
+namespace DonHang.Controllers
+{
+    public class POSController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListHoaDon(HoaDonSearchRepository repo)
+        {
+            repo.Page = 1;
+            repo.Execute(this);
+            return View();
+        }
+
+        //API
+        [HttpPost]
+        public ActionResult ExecuteSearch(HoaDonSearchRepository repo)
+        {
+            var list = repo.Execute(this);
+            return Json(list);
+        }
+    }
+}

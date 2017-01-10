e333bdd1011468ff0bc4158f8df195970f029005
trantien489 <tranviettien1996@gmail.com>
2017-01-11 01:13:19 +0700

    Thêm Lịch sử mua hàng

---
+++ TB/TB.MvcWebUser/Controllers/DonHangController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TB.Domain.EntityModel;
+
+namespace TB.MvcWebUser.Controllers
+{
+    public class DonHangController : Controller
+    {
+        private QLBH db = new QLBH();
+        // GET: DonHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListDonHang()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetThongtinDH(int DonHangId) {
+            var dh = db.DonHangs.Find(DonHangId);
+            var ListCtdh = db.ChiTietDonHangs.Where(p => p.DonHangId == dh.DonHangId).ToList();
+            var Ttnd = db.ThongTinNguoiDungs.Find(dh.KhachHangId);
+            return Json(
+                new
+                {
+                    DonHang = dh,
+                    ThongTinNguoiDung = Ttnd,
+                    ListCtdh= ListCtdh,  
+                    Ngaydat = dh.NgayLap.ToString()              
+                }
+            );
+        }
+    }
+}
